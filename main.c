eaa5408b706259f6932a848feb1625433d81c2d9
Kelvin Beilke <kelvinwesley66@hotmail.com>
2023-09-22 17:03:21 -0300

    Lindas alterações

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int login()
+{
+    printf("Escolha o usuario:\n");
+    printf("1. Administrador\n");
+    printf("2. Gerente\n");
+    printf("3. ")
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    login()
+}
